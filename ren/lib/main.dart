e6c90f6ff1165f26826509220b47a7669748c2ba
Dhiren <77841136+Dhirennn@users.noreply.github.com>
2023-12-29 01:36:57 +0800

    Added LoginPage UI
    - Added GUI for LoginPage.
    
    - Create widgets:
    i) Sign In Button Widget
    ii) Square Tile Widget
    iii) Login TextField Widget
    
    - TextField padding a little off, will fix.

--- ren/lib/main.dart
+++ ren/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:ren/pages/login_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -7,4 +8,15 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
 }
