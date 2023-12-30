6cb564798a2809cc22c446ec72a2520a8f4f719e
Dhiren <77841136+Dhirennn@users.noreply.github.com>
2023-12-30 18:20:59 +0800

    Changed NavBar design and fixed issues
    - fixed issue where navbar bgColor was white
    - added green pill, added selected icon change color to black
    - UI fixes to Navbar
    - added method to build navbar items
    - the 'label' argument is not needed but i left it there just in case need to use it in the future

--- ren/lib/main.dart
+++ ren/lib/main.dart
@@ -6,13 +6,14 @@ import 'package:ren/pages/splash_screen_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
-    WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -20,10 +21,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Roboto',
-        useMaterial3: true,
+        canvasColor: Colors.transparent,
       ),
-
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
--- ren/lib/pages/home_page.dart
+++ ren/lib/pages/home_page.dart
@@ -3,155 +3,89 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ren/pages/logged_in_pages/explore_page.dart';
 
-
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  var selectedItem = 0;
+class _HomePageState extends State<HomePage> {
+  int selectedItem = 0;
 
-  List children = [
+  List<Widget> children = [
     ExplorePage(),
     ExplorePage(),
     ExplorePage(),
     ExplorePage(),
     ExplorePage(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF202020),
       body: children[selectedItem],
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Color(0xFF1B1B1B),
-        selectedItemColor: Color(0xFF8E8E93),
-        iconSize: 27.0,
-        currentIndex: selectedItem,
-        unselectedLabelStyle: TextStyle(color: Color(0xFF1B1B1B)),
-        unselectedItemColor: Color(0xFF888888),
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        onTap: (currIndex) {
-          setState(() {
-            selectedItem = currIndex;
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-            backgroundColor: Color(0xFF1B1B1B),
-            icon: Stack( // Use Stack to place elements on top of each other
-              children: <Widget>[
-                Icon(Icons.explore),
-                Positioned( // Position your indicator or badge
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    height: 5.0,
-                    width: 5.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFB5DBAA),
-                    ),
-                  ),
-                ),
-              ],
+      bottomNavigationBar: Material(
+        color: Color(0xFF1B1B1B),
+        child: BottomNavigationBar(
+          backgroundColor: Color(0xFF1B1B1B),
+          selectedItemColor: Color(0xFF8E8E93),
+          unselectedItemColor: Color(0xFF8E8E93),
+          currentIndex: selectedItem,
+          showUnselectedLabels: true,
+          onTap: (currIndex) {
+            setState(() {
+              selectedItem = currIndex;
+            });
+          },
+          items: [
+            // ... other BottomNavigationBarItems
+        
+            // Profile BottomNavigationBarItem
+            BottomNavigationBarItem(
+              icon: _buildNavItem(Icons.search, 'Explore', selectedItem == 0),
+              label: 'Explore',
             ),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Color(0xFF1B1B1B),
-            icon: Stack( // Use Stack to place elements on top of each other
-              children: <Widget>[
-                Icon(Icons.people),
-                Positioned( // Position your indicator or badge
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    height: 5.0,
-                    width: 5.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFB5DBAA),
-                    ),
-                  ),
-                ),
-              ],
+            BottomNavigationBarItem(
+              icon: _buildNavItem(Icons.people, 'People', selectedItem == 1),
+              label: 'People',
             ),
-            label: 'Community',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Color(0xFF1B1B1B),
-            icon: Stack( // Use Stack to place elements on top of each other
-              children: <Widget>[
-                Icon(Icons.place),
-                Positioned( // Position your indicator or badge
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    height: 5.0,
-                    width: 5.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFB5DBAA),
-                    ),
-                  ),
-                ),
-              ],
+            BottomNavigationBarItem(
+              icon: _buildNavItem(Icons.place, 'Ren', selectedItem == 2),
+              label: 'Ren',
             ),
-            label: 'Ren',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Color(0xFF1B1B1B),
-            icon: Stack( // Use Stack to place elements on top of each other
-              children: <Widget>[
-                Icon(Icons.card_giftcard),
-                Positioned( // Position your indicator or badge
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    height: 5.0,
-                    width: 5.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFB5DBAA),
-                    ),
-                  ),
-                ),
-              ],
+            BottomNavigationBarItem(
+              icon: _buildNavItem(Icons.card_giftcard, 'Rewards', selectedItem == 3),
+              label: 'Rewards',
             ),
-            label: 'Rewards',
-          ),
-          BottomNavigationBarItem(
-            backgroundColor: Color(0xFF1B1B1B),
-            icon: Stack( // Use Stack to place elements on top of each other
-              children: <Widget>[
-                Icon(Icons.person),
-                Positioned( // Position your indicator or badge
-                  right: 0,
-                  top: 0,
-                  child: Container(
-                    height: 5.0,
-                    width: 5.0,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Color(0xFFB5DBAA),
-                    ),
-                  ),
-                ),
-              ],
+            BottomNavigationBarItem(
+              icon: _buildNavItem(Icons.person, 'Profile', selectedItem == 4),
+              label: 'Profile',
             ),
-            label: 'Profile',
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-}
 
-
+  Widget _buildNavItem(IconData iconData, String label, bool isSelected) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+      decoration: isSelected
+          ? BoxDecoration(
+              color: Color(0xFFB5DBAA), // Green background color
+              borderRadius: BorderRadius.circular(18), // Pill shape
+            )
+          : null, // No decoration when not selected
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(iconData,
+              size: 24, color: isSelected ? Colors.black : Color(0xFF8E8E93)),
+          ],
+        
+      ),
+    );
+  }
 
   // Sign user out method
   void userSignOut() async {
@@ -160,3 +94,4 @@ class _HomePageState extends State<HomePage>
     // sign out from google
     await GoogleSignIn().signOut();
   }
+}
