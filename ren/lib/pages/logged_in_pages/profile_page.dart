fe92d76d3717a63b02a32d9d768174521b11d656
TongJetKit <jton0028@student.monash.edu>
2024-01-01 00:44:41 +0800

    Added the leaderboard button and the achievements

--- ren/lib/pages/logged_in_pages/profile_page.dart
+++ ren/lib/pages/logged_in_pages/profile_page.dart
@@ -28,16 +28,90 @@ class ProfilePage extends StatelessWidget {
         elevation: 0,
         actions: [IconButton(onPressed: (){}, icon: Icon(Icons.settings))],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left:20),
-              child: ProfileInfo()
-              ),
-          ],
-        ),
-      ),
+      body: Center(
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+              children:[
+                ProfileInfo(),
+                Column(
+                  children: [
+                    Text('  82 days', textAlign: TextAlign.center, style: TextStyle(color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Roboto',),),
+                    Image.asset("lib/images/streak.png", height: 100, width: 100,),
+                    const SizedBox(height: 20,),
+                    Container(
+                      color: Colors.transparent,
+                      child:Column(
+                        children:[
+                          Container(
+                     
+                            child: Column(
+                              children: [
+                                Image.asset("lib/images/streak.png", height: 100, width: 100,),
+                                Text('1st place', textAlign: TextAlign.center, style: TextStyle(color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Roboto',),),
+                              ],
+                            ),
+                          ),
+                          
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Container(
+ 
+                                  child: Column(
+                                    children: [
+                                      Image.asset("lib/images/streak.png", height: 100, width: 100,),
+                                      Text('2nd place', textAlign: TextAlign.center, style: TextStyle(color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Roboto',),),
+                                    ] ,
+                                  ),
+                              ),
+                              Container(
+    
+                                child: Column(
+                                  children: [
+                                    Image.asset("lib/images/streak.png", height: 100, width: 100,),
+                                    Text('3rd place', textAlign: TextAlign.center, style: TextStyle(color: Colors.orange, fontSize: 20, fontWeight: FontWeight.bold, fontFamily: 'Roboto',),),
+                                  ],
+                                ),
+                              ),
+                            ]  
+                          ),
+                          const SizedBox(height: 50,),
+                          ElevatedButton(
+                            onPressed: (){}, 
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: Size(1000,50), //adjust accordingly
+                              backgroundColor:Color.fromARGB(255, 62, 62, 66),
+                              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                              side: BorderSide.none,
+                              shape: StadiumBorder(),
+                              textStyle: TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            child: Text("View Leaderboard", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontFamily: 'Roboto',),),
+                            
+                            )
+                        ]
+                          
+                      )
+                    ),
+                      
+
+
+                  ],
+                ),
+              ]
+                )
+                    
+                ),
+           
+              
+          
+          ),
+    
+      
     
 
     );
