6f0d5f6db78594ec79b0b963c14ded2ea09b698b
Dhiren <77841136+Dhirennn@users.noreply.github.com>
2024-01-02 21:44:19 +0800

    Add separate dart files for local and following tab

---
+++ ren/lib/components/following_tab_community_page.dart
@@ -0,0 +1,10 @@
+// following_tab_community_page.dart
+import 'package:flutter/material.dart';
+
+class FollowingTabCommunityPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with your actual following tab content
+    return Center(child: Text('Following Tab'));
+  }
+}
---
+++ ren/lib/components/local_tab_community_page.dart
@@ -0,0 +1,10 @@
+// local_tab_community_page.dart
+import 'package:flutter/material.dart';
+
+class LocalTabCommunityPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with your actual local tab content
+    return Center(child: Text('Local Tab'));
+  }
+}
